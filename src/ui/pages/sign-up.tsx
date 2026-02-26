323eee489946a7a2bfc169beb4a48e422d03030d
lucas vinicius <lucassilved@gmail.com>
2026-02-26 09:18:32 -0300

    feat: add visual for sign up page

---
+++ src/ui/pages/sign-up.tsx
@@ -0,0 +1,77 @@
+import logoImage from '@/ui/assets/logo.png';
+import { Button } from '@/ui/components/ui/button';
+import { Input } from '../components/ui/input';
+import { Label } from '../components/ui/label';
+
+interface BaseInputProps extends React.ComponentProps<'input'> {
+  label: string;
+}
+
+const BaseInput = (props: BaseInputProps) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <Label className="text-[16px] font-bold">{props.label}</Label>
+      <Input
+        className="h-10.5 rounded-b-md border-3 border-blue-500"
+        {...props}
+      />
+    </div>
+  );
+};
+
+export const SignUpPage = () => {
+  return (
+    <div className="grid h-screen w-screen grid-cols-[1fr_2fr] max-md:flex max-md:items-center max-md:justify-center">
+      <section className="flex h-full w-full items-center justify-center bg-blue-600 max-md:hidden">
+        <img
+          src={logoImage}
+          alt="Logo escrito vendas de água"
+          className="h-95 w-95"
+        />
+      </section>
+      <section className="flex w-full items-center justify-center">
+        <div className="w-full max-w-170 max-[1120px]:p-8">
+          <form>
+            <h2 className="mb-4 text-2xl font-bold">
+              Preencha com os seus dados
+            </h2>
+            <div className="flex flex-col gap-4">
+              <BaseInput
+                label="Nome"
+                type="text"
+                placeholder="Degite seu nome"
+              />
+              <BaseInput
+                label="Email"
+                type="email"
+                placeholder="Degite seu email"
+              />
+              <BaseInput
+                label="Senha"
+                type="password"
+                placeholder="Digite sua senha"
+              />
+              <BaseInput
+                label="Confirmar senha"
+                type="password"
+                placeholder="Confirme sua senha"
+              />
+
+              <div className="mt-4 w-full gap-2">
+                <div className="flex w-full justify-center">
+                  <div className="flex w-70 items-center justify-around text-[14px] text-neutral-500">
+                    <p>Já tenho uma conta</p>
+                  </div>
+                </div>
+
+                <Button className="mt-3 h-10.5 w-full cursor-pointer bg-blue-600 font-bold hover:bg-blue-500">
+                  CADASTRAR
+                </Button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+};
