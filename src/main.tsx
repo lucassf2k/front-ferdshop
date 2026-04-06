fe44bc3369303865ba3688b79b17ea1eb2bc21a3
lucas vinicius <lucassilved@gmail.com>
2026-04-06 16:28:04 -0300

    feat: add sonner

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
-import { FerdShopApp } from './ui/ferdshop-app';
+import { FerdShopApp } from '@/ui/ferdshop-app';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,28 @@
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter, Outlet } from 'react-router';
 import { RouterProvider } from 'react-router/dom';
 import { SignInPage } from '@/ui/pages/sign-in';
 import { SignUpPage } from '@/ui/pages/sign-up';
+import { Toaster } from '@/ui/components/ui/sonner';
 
-export const router = createBrowserRouter([
+const RootLayout = () => {
+  return (
+    <>
+      <Outlet />
+      <Toaster richColors theme="light" position="top-center" />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
   {
     path: '/',
-    element: <SignInPage />,
-  },
-  {
-    path: '/sign-up',
-    element: <SignUpPage />,
-  },
-  {
-    path: '/test',
-    element: <div>test</div>,
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <SignInPage /> },
+      { path: 'sign-up', element: <SignUpPage /> },
+      { path: 'test', element: <div>test</div> },
+    ],
   },
 ]);
 
-export const Router = () => {
-  return <RouterProvider router={router} />;
-};
+export const Router = () => <RouterProvider router={router} />;
