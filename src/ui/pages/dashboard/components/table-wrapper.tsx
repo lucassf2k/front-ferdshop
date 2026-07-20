b5701d0f6af5487e6fd159f79cbfea6ba9a1db08
lucas vinicius <lucassilved@gmail.com>
2026-07-20 20:19:26 -0300

    refactor: add wrapper table dashboard

--- src/ui/pages/dashboard/components/table-wrapper.tsx
+++ src/ui/pages/dashboard/components/table-wrapper.tsx
@@ -1,9 +1,15 @@
-import type { PropsWithChildren } from 'react';
-import { RegisterProductDialog } from './register-product';
 import { Link } from 'react-router';
 import { IoIosArrowForward } from 'react-icons/io';
 
-export const TableWrapper = ({ children }: PropsWithChildren) => {
+interface Props {
+  title: string;
+  children: React.ReactNode;
+  createComp?: React.ReactNode;
+}
+
+export const TableWrapper = ({ children, title, createComp }: Props) => {
+  const CreateComp = createComp ? createComp : null;
+
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="flex h-60 w-full bg-amber-500" />
@@ -12,11 +18,12 @@ export const TableWrapper = ({ children }: PropsWithChildren) => {
           to="/"
           className="flex items-center gap-1 text-[12px] font-semibold text-white"
         >
-          Home <IoIosArrowForward /> <span className="underline">Produtos</span>
+          Home <IoIosArrowForward />{' '}
+          <span className="lowercase underline">{title}</span>
         </Link>
         <div className="mb-8 flex items-center justify-between">
-          <h3 className="text-4xl font-bold text-white">Produtos</h3>
-          <RegisterProductDialog />
+          <h3 className="text-4xl font-bold text-white">{title}</h3>
+          {CreateComp}
         </div>
         <div className="rounded-md border bg-white py-2">{children}</div>
       </div>
